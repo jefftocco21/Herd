ff5572b80640aacf3abef852bc84cf0c2026cdf0
Jeff Tocco <jnt369@gmail.com>
2025-05-21 13:20:22 -0400

    Basic validation on client and server side

--- example/resources/views/jobs/create.blade.php
+++ example/resources/views/jobs/create.blade.php
@@ -5,7 +5,7 @@
     <form method="POST" action="/jobs">
         @csrf 
 
-        
+
 
         <div class="space-y-12">
           <div class="border-b border-gray-900/10 pb-12">
@@ -17,8 +17,12 @@
                 <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                 <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Shift Leader">
+                    <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Shift Leader" required>
                   </div>
+
+                  @error('title')
+                    <p class = "text-xs text-red-500 font-semibold mt-1"> {{$message}} </p>
+                  @enderror
                 </div>
               </div>
       
@@ -26,11 +30,26 @@
                 <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                 <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="$50,00 per year">
+                    <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="$50,00 per year" required>
                   </div>
+
+                  @error('salary')
+                  <p class = "text-xs text-red-500 font-semibold mt-1"> {{$message}} </p>
+                @enderror
+
                 </div>
-          </div>
-      
+{{-- 
+                <div>
+                    @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="text-red-500 italic">{{$error}}</li>
+                        @endforeach
+                    </ul>
+                @endif
+                </div> --}}
+
+            </div>
         </div>
       
         <div class="mt-6 flex items-center justify-end gap-x-6">
