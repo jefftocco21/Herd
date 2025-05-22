a404c52d3e21ebeb899777a1d33753fbe9519064
Jeff Tocco <jnt369@gmail.com>
2025-05-22 13:41:24 -0400

    Edit, Update, Delete a resource with no authentication

--- example/routes/web.php
+++ example/routes/web.php
@@ -6,6 +6,7 @@ use App\Models\Job;
 Route::get('/', function () {
 });
 
+//Index
 Route::get('/jobs', function (){
     $jobs= Job::with('employer')->latest()->paginate(5);
 
@@ -14,16 +15,19 @@ Route::get('/jobs', function (){
     ]);
 });
 
+//create
 Route::get('jobs/create', function () {
      return view('jobs.create');
 });
 
+//show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job ]);   
 });
 
+//store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -37,6 +41,42 @@ Route::post('/jobs', function () {
     ]);
 
     return redirect('jobs/');
+});
+
+//edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job ]);   
+});
+
+//update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+    //authorize (not yet)
+    //update job
+    $job = Job::findorFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+    
+    //persist
+    //redirect to job page
+    return redirect('/jobs/' . $job->id);
+});
+
+//destroy
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize
+    //delete job
+    $job = Job::findorFail($id)->delete();
+    //redirect
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
