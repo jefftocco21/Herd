96bd8466ad1ef7cf42859f8b0e373787ae08bb5a
Jeff Tocco <jnt369@gmail.com>
2025-06-05 13:40:06 -0400

    register view

---
+++ example/resources/views/auth/register.blade.php
@@ -0,0 +1,81 @@
+<x-layout>
+    <x-slot:heading>
+        Register
+    </x-slot:heading>
+    <form method="POST" action="/jobs">
+        @csrf 
+
+            <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <x-form-field>
+                  <x-form-label for="first_name">First Name</x-form-label>
+                <div class="mt-2">
+
+                    <x-form-input name="first_name" id="first_Name" required/>
+                  
+                    <x-form-error name="first_name" />
+
+                </div>
+              </x-form-field>
+
+              <x-form-field>
+                <x-form-label for="last_name">Last Name</x-form-label>
+              <div class="mt-2">
+
+                  <x-form-input name="last_name" id="last_name" required/>
+                
+                  <x-form-error name="last_name" />
+
+              </div>
+            </x-form-field>
+
+            <x-form-field>
+                <x-form-label for="email">Email</x-form-label>
+              <div class="mt-2">
+
+                  <x-form-input name="email" id="email" type="email" required/>
+                
+                  <x-form-error name="email" />
+
+              </div>
+            </x-form-field>
+
+            <x-form-field>
+                <x-form-label for="password">Password</x-form-label>
+              <div class="mt-2">
+
+                  <x-form-input name="password" id="password" type="password" required/>
+                  <x-form-error name="password" required/>
+
+              </div>
+            </x-form-field>
+
+            <x-form-field>
+                <x-form-label for="password_confirmation">Confirm Password</x-form-label>
+              <div class="mt-2">
+
+                  <x-form-input name="password_confirmation" id="password_confirmation" type="password" required/>
+                  <x-form-error name="password_confirmation" />
+
+              </div>
+            </x-form-field>
+      
+                <div>
+                    @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="text-red-500 italic">{{$error}}</li>
+                        @endforeach
+                    </ul>
+                @endif
+                </div>
+
+            </div>
+        </div>
+      
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+          <a href="/" class="text-sm font-semibold text-gray-900">Cancel</button>
+          <x-form-button>Register</x-form-button>
+        </div>
+      </form>
+      
+</x-layout>
--- example/routes/web.php
+++ example/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
-use App\Http\Controllers\JobController;
-use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+
+//Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
