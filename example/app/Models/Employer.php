e4dbbbb380c761bf0ba7641617c308506a222100
Jeff Tocco <jnt369@gmail.com>
2025-06-18 13:20:52 -0400

    Gates & Can for editing jobs

--- example/app/Models/Employer.php
+++ example/app/Models/Employer.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employer extends Model
 {
@@ -13,5 +15,10 @@ class Employer extends Model
     public function jobs()
     {
         return $this->hasMany(Job::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
