75ed4e23f3e39d21e6ad2ea23546341b55927ee8
Jeff Tocco <jnt369@gmail.com>
2025-06-24 13:59:05 -0400

    Send an email once a job is created

--- example/resources/views/mail/job-posted.blade.php
+++ example/resources/views/mail/job-posted.blade.php
@@ -1,1 +1,10 @@
-Congrats! Job is now live on the website.
+<h2>
+    {{$job->title}}
+</h2>
+<p>
+    Congrats! Job is now live on the website.
+</p>
+
+<p>
+    <a href="{{ url('/jobs/' . $job->id) }}">View Your Job Listing </a>
+</p>
